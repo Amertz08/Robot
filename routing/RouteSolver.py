f715d9c100d0ac93a7abb080a28e9e88784ecc0e
Collin Duddy <collin.duddy@gmail.com>
2018-01-30 11:29:40 -0600

    Final code revisions
    These are the changes for the most recent comments on the file.

--- routing/RouteSolver.py
+++ routing/RouteSolver.py
@@ -27,30 +27,32 @@ DIRECTIONS = ('N', 'S', 'E', 'W')
 ##################################################################################
 
 def load_config(config_file):
-    with open(config_file,"r") as ymlfile:
+    with open(config_file, "r") as ymlfile:
         cfg = yaml.safe_load(ymlfile)
-    curr_node = ""
-    bad_chars = '(){}<>[]'
-    print(cfg)
     for node in cfg['nodes']:
-        print(node)
         graph.add_vertex(node['node']['name'])
         for connected_nodes in node['node']['connected_nodes']:
-            print(connected_nodes)
             graph.add_edge((node['node']['name'], connected_nodes['connected_node']))
             #Add directional support here
 
+def get_node(type):
+    while True:
+        node = input(f'Please enter the {type} node:\n')
+        if node not in graph.vertices():
+            print(f'Invalid {type} node provided\n')
+            continue
+        return node
+
 def bool_input(message):
-    '''From Adam Mertz'''
     '''Gets y/n input for given message'''
     while True:
-        ans = input("{message} (y/n): ")
+        ans = input(f"{message} (y/n): ")
         if ans.lower() == 'y':
             return True
         elif ans.lower() == 'n':
             return False
         else:
-            print("Invalid input: {ans} - y/n or Y/N only")
+            print(f"Invalid input: {ans} - y/n or Y/N only")
 
 def get_graph_details_from_user():
     number_of_nodes = 0;
@@ -62,52 +64,37 @@ def get_graph_details_from_user():
             print('Invalid input. Please input an integer')
             continue
     node_count = 0
-    while node_count < int(number_of_nodes):
-        new_name = input("Please enter a name for node {node_count}\n")
+    while node_count < number_of_nodes:
+        new_name = input(f"Please enter a name for node {node_count}\n")
         graph.add_vertex(new_name)
         node_count += 1
     print(graph.vertices())
     for node in graph.vertices():
         another_node = True
         while another_node:
-            node_name = input("Please enter connected node name for node {node}:\n")
+            node_name = input(f"Please enter connected node name for node {node}:\n")
             if node_name in graph.vertices():
                 while True:
-                    direction = input("Please enter the cardinal direction traveling from {node} to {node_name}: {DIRECTIONS}\n")
+                    direction = input(f"Please enter the cardinal direction traveling from {node} to {node_name}: {DIRECTIONS}\n")
                     if direction in DIRECTIONS:
                         graph.add_edge((node, node_name))
-                        another_connection = bool_input("Is there another connected node?")
-                        if another_connection:
-                            another_node = True
-                        else :
-                            another_node = False
+                        another_node = bool_input("Is there another connected node?")
                         break
                     else:
-                        print('Invalid direction given. Valid directions ({DIRECTIONS})')
+                        print(f'Invalid direction given. Valid directions ({DIRECTIONS})')
             else:
-                connected = bool_input("Invalid node name given. Is there a node connected?")
-                if connected:
-                    another_node = True
-                else:
-                    another_node = False
+                another_node = bool_input("Invalid node name given. Is there a node connected?")
 
-def main(args):
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", help = "Optional config file to streamline graph creation.")
+    args = parser.parse_args()
     if args.c:
         load_config(args.c)
     else:
         get_graph_details_from_user()
-    while True:
-        start_node = input("Please enter the start node:\n")
-        if start_node in graph.vertices() :
-            break;
-        else:
-            print("Invalid start node provided\n")
-    while True:
-        end_node = input("Please enter the end node:\n")
-        if end_node in graph.vertices():
-            break;
-        else:
-            print("Invalid end node provided\n")
+    start_node = get_node('start')
+    end_node = get_node('end')
     paths = graph.find_path(start_node, end_node)
     print("This is a single path: ")
     print(paths)
@@ -123,7 +110,4 @@ def main(args):
 ##                                                                              ##
 ##################################################################################
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", help = "Optional config file to streamline graph creation.")
-    args = parser.parse_args()
-    main(args)
+    main()
