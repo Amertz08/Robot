3347b9cae05b1049c3cafd4eea9b76cf210bf891
Adam Mertz <adammertz@gmail.com>
2018-02-01 13:30:38 -0600

    basic keyboard script

--- arduino/drive/Monster/Monster.ino
+++ arduino/drive/Monster/Monster.ino
@@ -1,11 +1,34 @@
 #include "Monster.h"
 
+#define SPEED 120
+
+Monster shield = Monster(MIXED);
+
 void setup() {
-  // put your setup code here, to run once:
-  Monster shieldOne = Monster(MIXED);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    char inChar = Serial.read();
+
+    switch (inChar) {
+      case 'W':
+        shield.forward(SPEED);
+        break;
+      case 'S':
+        shield.backward(SPEED);
+        break;
+      case 'D':
+        shield.driveLeft(SPEED);
+        break;
+      case 'A':
+        shield.driveRight(SPEED);
+        break;
+      default:
+        shield.stop();
+        break;
+    }
+  }
 
 }
