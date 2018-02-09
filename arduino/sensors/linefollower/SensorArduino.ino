39b604be462bd9eea3cd97b60df52620b2047996
nBukaty <nickbukaty@gmail.com>
2018-02-09 10:50:30 -0600

    Added stop on obstacle detection
    Using the ultrasonic sensor we can detect obstacles in the way of the
    robot and stop

--- arduino/sensors/linefollower/SensorArduino.ino
+++ arduino/sensors/linefollower/SensorArduino.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include "sensorbar.h"
+#include "smoothing.h" //not sure how we're including with the make file
 
 #define motor_arduino_address 8
 
@@ -8,6 +9,10 @@
 #define GO_FORWARD 2
 #define GO_LEFT 3
 #define GO_RIGHT 4
+
+//Pins
+const unit8_t TRIG_PIN = 12;
+const unit8_t ECHO_PIN = 13;
 
 const uint8_t SX1509_ADDRESS = 0x3E;
 
@@ -23,13 +28,19 @@ uint8_t m_rightSpeed;
 const uint8_t FWD = 0;
 const uint8_t BKWD = 1;
 const uint8_t MAX_SPEED = 150;
+const uint8_t MAX_OBSTACLE_DISTANCE = 8;
 
 SensorBar mySensorBar(SX1509_ADDRESS);
+SMOOTHING ultraSmooth = SMOOTHING(5);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Program started.");
   Serial.println();
+
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+
   mySensorBar.setBarStrobe();
   mySensorBar.clearInvertBits();
   returnStatus = mySensorBar.begin();
@@ -40,6 +51,7 @@ void setup() {
     Serial.println("sx1509 IC communication FAILED!");
   }
   Serial.println();
+  
   Wire.begin(10);
   m_leftDirection = FWD;
   m_leftSpeed = 0;
@@ -60,20 +72,26 @@ void loop() {
     prevState = IDLE_STATE;
     break;
   case READ_LINE:
-    if(mySensorBar.getDensity() < 8 && mySensorBar.getDensity() > 0) {
-      nextState = GO_FORWARD;
-      if(mySensorBar.getPosition() < -50) {
-        nextState = GO_LEFT;
+    if(ultraSonic() > MAX_OBSTACLE_DISTANCE) {
+      if(mySensorBar.getDensity() < 8 && mySensorBar.getDensity() > 0) {
+        nextState = GO_FORWARD;
+        if(mySensorBar.getPosition() < -50) {
+          nextState = GO_LEFT;
+        }
+        if(mySensorBar.getPosition() > 50) {
+          nextState = GO_RIGHT;
+        }
       }
-      if(mySensorBar.getPosition() > 50) {
-        nextState = GO_RIGHT;
+      else if(mySensorBar.getDensity() == 0) {
+        nextState = findLine();
       }
-    }
-    else if(mySensorBar.getDensity() == 0) {
-      nextState = findLine();
+      else {
+        nextState = IDLE_STATE;
+      }
     }
     else {
-      nextState = IDLE_STATE;
+      stopMotors();
+      nextState = READ_LINE;
     }
     prevState = READ_LINE;
     break;
@@ -145,4 +163,32 @@ uint8_t findLine() {
   if(mySensorBar.getPosition() > 50) {
         return GO_RIGHT;
   }
+}
+
+uint16_t ultraSonic() {
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  uint16_t cm;
+  uint16_t average;
+
+  /* Hold the trigger pin high for at least 10 us */
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  /* Wait for pulse on echo pin */
+  while ( digitalRead(ECHO_PIN) == 0 );
+
+  /*
+  * Measure how long the echo pin was held high (pulse width)
+  * Note: the micros() counter will overflow after ~70 min
+  */
+  t1 = micros();
+  while ( digitalRead(ECHO_PIN) == 1);
+  t2 = micros();
+  pulse_width = t2 - t1;
+  cm = pulse_width / 58;
+  average = ultraSmooth.smooth(cm);
+  return average;
 }
