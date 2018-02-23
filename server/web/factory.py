1c8b8ded6a4a19141911aa4fa45de6f6010fefe6
HHuang <huanghaozhan@gmail.com>
2018-02-23 09:02:17 -0600

    Create models

--- server/web/factory.py
+++ server/web/factory.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 from config import config
+
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
---
+++ server/web/models.py
@@ -0,0 +1,20 @@
+from . import db
+
+class Account(db.Model):
+    __tablename__ = 'account'
+    id = db.Column(db.Integer, primary_key=True)
+    companyName = db.Column(db.String(64), unique=True)
+    createDate = db.Column(db.DateTime)
+    active = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<Account %r>' % self.companyName
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    acct_id = db.Column(db.String(64)) #still need set this to be foreign key
+    firstName = db.Column(db.String(64))
+    lastName = db.Column(db.String(64))
+    email = db.Column(db.String(64))
+    password = db.Column(db.String(64))
