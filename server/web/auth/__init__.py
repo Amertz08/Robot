07313a8b8d093d989c561cb15577f6e622c895d5
Adam Mertz <adammertz@gmail.com>
2018-02-27 15:38:04 -0600

    Actually logged in user and created logout view

--- server/web/auth/__init__.py
+++ server/web/auth/__init__.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_login import login_user, login_required
 
 from forms import LoginForm
+from models import db, User
 
 auth = Blueprint('auth', __name__)
 
@@ -8,5 +10,15 @@ auth = Blueprint('auth', __name__)
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        pass
+        user = User.query.filter_by(email=form.email.data).first()
+        login_user(user)
+        flash('Login Successful', 'success')
+        return redirect(url_for('main.index')) # TODO: fix redirect location
     return render_template('auth/login.html.j2', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out', 'success')
+    return redirect(url_for('main.index'))
