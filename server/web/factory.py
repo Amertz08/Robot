88c3d3fd19e3012621e8f4785aded7164ac3900c
Adam Mertz <adammertz@gmail.com>
2018-02-27 16:06:32 -0600

    Properly init sqlalchemy instance

--- server/web/factory.py
+++ server/web/factory.py
@@ -9,6 +9,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    db.init_app(app)
     Bootstrap(app)
     login_manager = LoginManager(app)
     login_manager.login_view = 'auth.login'
