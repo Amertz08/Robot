9ec80e3b3af24b8c6c13575d52537c90ac1648b6
Adam Mertz <adammertz@gmail.com>
2018-02-27 15:18:50 -0600

    Created basic form and view

--- server/web/auth/__init__.py
+++ server/web/auth/__init__.py
@@ -1,3 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from forms import LoginForm
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('auth/login.html.j2', form=form)
