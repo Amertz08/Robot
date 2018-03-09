d45a68164c9fcf5a70319384c1779bb6263d8793
Adam Mertz <adammertz@gmail.com>
2018-03-09 11:10:43 -0600

    Created login/logout methods

--- server/web/tests.py
+++ server/web/tests.py
@@ -2,6 +2,7 @@ import unittest
 
 from flask import url_for
 from flask_testing import TestCase
+from flask_login import login_user
 
 from factory import create_app
 from models import db, User, Account
@@ -17,6 +18,16 @@ class BaseTest(TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+
+    def login(self, email, password, follow=True):
+        return self.client.post(
+            url_for('auth.login'),
+            data={'email': email, 'password': password},
+            follow_redirects=follow
+        )
+
+    def logout(self, follow=True):
+        return self.client.get(url_for('auth.logout'), follow_redirects=follow)
 
     @staticmethod
     def add_acct(company_name):
@@ -45,8 +56,29 @@ class TestMain(BaseTest):
         self.assert200(resp)
         self.assert_template_used('main/index.html.j2')
 
+    def test_navbar_logged_out_user(self):
+        resp = self.client.get(url_for('main.index'))
+        self.assertIn(b'Login</a>', resp.data, 'Login link should appear')
+        self.assertIn(b'Sign Up</a>', resp.data, 'Sign Up link should appear')
+
+    def test_navbar_logged_in_user(self):
+        acct = self.add_acct('Test Company')
+        user = self.add_user(acct.id, 'Adam', 'Test', 'adam@example.com', 'pass')
+        self.login(user.email, 'pass')
+        resp = self.client.get(url_for('main.index'))
+        self.assertIn(b'Logout</a>', resp.data, 'Logout link should appear')
+        self.assertNotIn(b'Login</a>', resp.data, 'Login link should not appear')
+
 
 class TestAuth(BaseTest):
+
+    def test_login_logout(self):
+        acct = self.add_acct('Test Company')
+        user = self.add_user(acct.id, 'Adam', 'Test', 'adam@example.com', 'pass')
+        resp = self.login(user.email, 'pass')
+        self.assertIn(b'Login Successful', resp.data, 'Successful login message did not appear')
+        resp = self.logout()
+        self.assertIn(b'Logged out', resp.data, 'Logout message did not appear')
 
     def test_login_basic(self):
         resp = self.client.get(url_for('auth.login'))
@@ -56,11 +88,7 @@ class TestAuth(BaseTest):
         self.assertIn(b'>Reset Password</a>', resp.data, 'Reset password link missing')
 
     def test_login_no_user(self):
-        data = {
-            'email': 'adam@example.com',
-            'password': 'pass'
-        }
-        resp = self.client.post(url_for('auth.login'), data=data)
+        resp = self.login('adam@example.com', 'pass')
         self.assert200(resp)
         self.assertIn(b'Invalid Email/Password', resp.data)
 
@@ -68,12 +96,7 @@ class TestAuth(BaseTest):
         acct = self.add_acct('Test Company')
         user = self.add_user(acct.id, 'Adam', 'Test', 'adam@example.com', 'pass')
 
-        data = {
-            'email': 'adam@example.com',
-            'password': 'wrong'
-        }
-
-        resp = self.client.post(url_for('auth.login'), data=data)
+        resp = self.login('adam@example.com', 'wrong')
         self.assert200(resp)
         self.assertIn(b'Invalid Email/Password', resp.data)
 
@@ -81,28 +104,8 @@ class TestAuth(BaseTest):
         acct = self.add_acct('Test Company')
         user = self.add_user(acct.id, 'Adam', 'Test', 'adam@example.com', 'pass')
 
-        data = {
-            'email': 'adam@example.com',
-            'password': 'pass'
-        }
-
-        resp = self.client.post(url_for('auth.login'), data=data)
+        resp = self.login('adam@example.com', 'pass', False)
         self.assertRedirects(resp, url_for('main.index'), 'Should redirect to main.index')
-
-    def test_login_valid_user_valid_pw_follow(self):
-        acct = self.add_acct('Test Company')
-        user = self.add_user(acct.id, 'Adam', 'Test', 'adam@example.com', 'pass')
-
-        data = {
-            'email': 'adam@example.com',
-            'password': 'pass'
-        }
-
-        resp = self.client.post(url_for('auth.login'), data=data, follow_redirects=True)
-        self.assert200(resp)
-        self.assertIn(b'Login Successful', resp.data, 'Login Successful message missing')
-        self.assertIn(b'Logout</a>', resp.data, 'Logout link should appear')
-        self.assertNotIn(b'Login</a>', resp.data, 'Login link should not appear')
 
 
 
