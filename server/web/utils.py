e01bcd8bef1c83345a2bb0fe6f10babdf0629ad8
HHuang <huanghaozhan@gmail.com>
2018-03-09 12:30:00 -0600

    Changes, add confirm.html

--- server/web/utils.py
+++ server/web/utils.py
@@ -14,10 +14,9 @@ def send_email(to, subject, template, **kwargs):
     if isinstance(to, str):
         to = [to]
     app = current_app._get_current_object()
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=to)
-    msg.body = render_template(template + '.txt.j2', **kwargs)
-    msg.html = render_template(template + '.html.j2', **kwargs)
+    msg = Message(subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients=to)
+    msg.body = render_template(f'email/{template}.txt.j2', **kwargs)
+    msg.html = render_template(f'email/{template}.html.j2', **kwargs)
     thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
