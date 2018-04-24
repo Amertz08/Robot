9f3a482a89e3b676bc4062b509b4a46222f3dd08
HHuang <huanghaozhan@gmail.com>
2018-04-24 03:15:46 -0500

    Change add and remove user view function

--- server/web/account/__init__.py
+++ server/web/account/__init__.py
@@ -1,5 +1,5 @@
 import datetime
-from flask import Blueprint, url_for, flash, render_template, redirect, request, abort
+from flask import Blueprint, url_for, flash, render_template, redirect, request, abort, jsonify
 from forms import AddUserForm, RemoveUserForm, SetPassword
 from models import db, User
 from utils import send_email, print_debug, log_message
@@ -9,7 +9,21 @@ from itsdangerous import SignatureExpired, BadSignature
 acct = Blueprint('acct', __name__)
 
 
-@acct.route('/add-user', methods=['GET', 'POST'])
+@acct.route('/', methods=['GET', 'POST'])
+@login_required
+def index():
+    add_form = AddUserForm()
+    rm_form = RemoveUserForm()
+    users = User.query.filter(User.acct_id == current_user.acct_id, User.id != current_user.id).all()
+    ctx = {
+        'add_form': add_form,
+        'rm_form': rm_form,
+        'users': users
+    }
+    return render_template('account/index.html.j2', **ctx)
+
+
+@acct.route('/add-user', methods=['POST'])
 @login_required
 def add_user():
     form = AddUserForm()
@@ -30,25 +44,24 @@ def add_user():
         flash('New user has been added. A confirmation email is sent to user\'s email address. \
                 The link will expire after 24 hours.')
         log_message(f'User: {user.last_name}, {user.first_name} has been added')
-        return redirect(url_for('dash.index'))
-    return render_template('account/add-user.html.j2', form=form)
+        return jsonify('OK'), 201
+    else:
+        return jsonify(form.errors)
 
 
-@acct.route('/remove-user', methods=['GET', 'POST'])
+@acct.route('/remove-user', methods=['POST'])
 @login_required
 def rm_user():
     form = RemoveUserForm()
     if form.validate_on_submit():
         user = User.get(form.email.data)
-        if user:
-            db.session.delete(user)
-            db.session.commit()
-            log_message(f'acct_id: {user.acct_id},\t user: {user.first_name} has been removed')
-            flash('User has been removed')
-        else:
-            flash('User doesn\'t exist')
-        return redirect(url_for('dash.index'))
-    return render_template('account/rm-user.html.j2', form=form)
+        db.session.delete(user)
+        db.session.commit()
+        log_message(f'acct_id: {user.acct_id},\t user: {user.first_name} has been removed')
+        flash('User has been removed')
+        return jsonify('OK')
+    else:
+        return jsonify(form.errors)
 
 
 @acct.route('/set-pw', methods=['GET', 'POST'])
